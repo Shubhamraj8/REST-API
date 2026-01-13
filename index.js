e7edfbbab95649d0db38cc8545618c4df7768c2f
Shubham Raj Rathaur <94780982+Shubhamraj8@users.noreply.github.com>
2026-01-13 08:39:19 +0000

    used status codes

--- index.js
+++ index.js
@@ -23,22 +23,44 @@ app.get('/api/users', (req,res) => {
     return res.json(users);
 })
 
-app.get("/api/users/:id", (req, res) => { //:id = dynamic
+app
+.route("/api/users/:id")
+.get((req, res) => { //:id = dynamic
     const id = Number(req.params.id);
     const user = users.find((user) => user.id === id);
+    if(!user) return res.status(404).json({ error:"user not found"});
     return res.json(user);
+})
+
+//PATCH
+.patch((req, res) => {
+    return res.json({ status: "Pending" });
+})
+
+//DELETE
+.delete((req, res) => {
+    return res.json({ status: "Pending" });
 })
 
 //POST
 
 app.post("/api/users", (req,res) => {
     const body = req.body;
+    if(
+        !body ||
+        !body.first_name ||
+        !body.last_name ||
+        !body.email ||
+        !body.gender ||
+        !body.job_title
+    ){
+        return res.status(400).json({ msg: "All fields are required"});
+    }
     users.push({...body, id: users.length+1});
     fs.writeFile("./MOCK_DATA.json", JSON.stringify(users), (err, data) => {
-        return res.json({status: "success", id: users.length});
+        return res.status(201).json({status: "success", id: users.length});
     })
 })
-//PATCH
-//DELETE
+
 
 app.listen(PORT, () => console.log(`Server Started at PORT:${PORT}`));
